f17dcc6d897ad6fda9c01551e2a5444e0ebd8be2
David Etuk <djetuk@gmail.com>
2025-07-12 15:47:33 +0100

    added default backend message

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import app from './app';
+import app from "./app";
 
 const PORT = process.env.PORT || 4000;
+
+app.get("/", (_, res) => {
+  res.send("Backend is running ✅");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
